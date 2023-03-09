c1e9c68aba72bad8e89e2a74d83561416c177a92
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-09 12:36:21 -0800

    Up to part 3

--- app.py
+++ app.py
@@ -7,3 +7,24 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def display_surve_start():
+    return render_template('survey_start.html',
+                           title=survey.title,
+                           instructions=survey.instructions
+                           )
+
+@app.get('/begin')
+def redirect_to_question():
+    return redirect("/questions/0")
+
+@app.get('/questions/<id>')
+def questions_survey(id):
+
+    id = int(id)
+    return render_template('question.html',
+                           question=survey.questions[id]) 
+    # return ('hey')
