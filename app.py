357700d6cf42fcabb9d9502fb5f724abe595b73a
Claudia Lam <claudiaslam@gmail.com>
2023-03-09 17:27:58 -0800

    add routes to questions and completion page

--- app.py
+++ app.py
@@ -11,20 +11,43 @@ debug = DebugToolbarExtension(app)
 responses = []
 
 @app.get('/')
-def display_surve_start():
+def display_survey_start():
+    """display root page with title, instructions, and start survey button"""
     return render_template('survey_start.html',
                            title=survey.title,
                            instructions=survey.instructions
                            )
 
-@app.get('/begin')
+@app.post('/begin')
 def redirect_to_question():
+    """redirect /begin to first question"""
+    responses.clear()
     return redirect("/questions/0")
 
-@app.get('/questions/<id>')
+@app.get('/questions/<int:id>')
 def questions_survey(id):
+    """display survey question with answer submissions""" #with answer options
 
-    id = int(id)
+    # id = int(id)
     return render_template('question.html',
-                           question=survey.questions[id]) 
-    # return ('hey')
+                           question=survey.questions[id])
+
+@app.post('/answer')
+def handle_answer():
+    """save answers and redirects page to next question or completion page"""
+    answer = request.form['answer']
+    responses.append(answer)
+
+    if len(responses) != len(survey.questions):
+        return redirect(f"/questions/{len(responses)}")
+    else:
+        return redirect('/completion')
+
+@app.get('/completion')
+def complete_survey():
+    """display questions and responses"""
+    return render_template('completion.html', questions=survey.questions,
+                            responses=responses)
+
+
+
